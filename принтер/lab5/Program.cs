71d921c65356e5bb3041be0ef46a9bd8e4073493
Александра <112776816+SaiALL441@users.noreply.github.com>
2022-10-23 15:45:59 +0300

    Add files via upload

---
+++ принтер/lab5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab5
+{
+    public class Printer
+    {
+        public virtual void Print (string value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    public class Detail:Printer
+    {
+        public override void Print (string value)
+        {
+            var color = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value.GetType());
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Printer det = new Detail();
+            det.Print(" ... ");
+        }
+    }
+}
